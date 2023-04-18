d0a4dad0f0205086580579aa80c834a85d933e10
Fernando Perera <fernando02perera@gmail.com>
2023-04-18 11:53:02 +0200

    Test(green): should return multiple warning if the variable of dictionary is not serializable and if other variable dont exist in text test passed.

--- src/test/TemplateEngine.test.ts
+++ src/test/TemplateEngine.test.ts
@@ -10,11 +10,13 @@ import { TemplateEngineService } from '../core/services/TemplateEngineService';
  *               - 'this is a text for example'
  * 
  * 'this is a ${variable} for ${variable2}'  |  { variable: 'text', variable3: 'example' }
- *               - 'this is a text for ${variable2}     [warning]
+ *               - 'this is a text for ${variable2}'     [warning]
  * 
- *  'this is a ${variable} for ${variable2}'  |  { variable: 'text', variable2: null }
- *               - 'this is a text for ${variable2}     [warning]
+ *  'this is a ${variable} for ${variable2}'  |  { variable: 'text', variable2: undefined }
+ *               - 'this is a text for ${variable2}'     [warning]
  * 
+ * 'this is a ${variable} for ${variable2} and ${variable4}'  |  { variable: 'text', variable2: null, variable4: 'learn', variable7: 'test' }
+ *               - 'this is a text for ${variable2} and learn'     [warning]
 */
 
 describe('Template engine test', () => {
@@ -69,6 +71,23 @@ describe('Template engine test', () => {
         const variableDictionary: Dictionary = Dictionary.create({ variable: 'text', variable2: undefined})
         const expectedRespose: string = 'this is a text for ${variable2}'
         const expectedWarning: string = "warning : [{wrongKey:variable2,reason:variable is not serializable}]"
+
+        const result: string = templateEngineService.replaceVariable(textToReplace, variableDictionary)
+
+        expect(warn).toBeCalledWith(expectedWarning)
+        expect(result).toBe(expectedRespose)
+
+        warn.mockReset()
+
+    })
+
+    it('should return multiple warning if the variable of dictionary is not serializable and if other variable dont exist in text', () => {
+
+        const warn = jest.spyOn(console, "warn").mockImplementation(() => {})
+        const textToReplace: TextToReplace = TextToReplace.create('this is a ${variable} for ${variable2} and ${variable4}')
+        const variableDictionary: Dictionary = Dictionary.create({ variable: 'text', variable2: null, variable4: 'learn', variable7: 'test' })
+        const expectedRespose: string = 'this is a text for ${variable2} and learn'
+        const expectedWarning: string = "warning : [{wrongKey:variable2,reason:variable is not serializable},{wrongKey:variable7,reason:variable dont exist in text}]"
 
         const result: string = templateEngineService.replaceVariable(textToReplace, variableDictionary)
 
